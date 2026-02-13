2bb5a20dffb1b9fab5d91de76b1913a8d273ba99
25j023_yinmaroo <25j023_yinmaroo@edu.kimura.ac.jp>
2026-02-13 13:44:01 +0900

    initial commit

--- src/App.java
+++ src/App.java
@@ -1,7 +1,12 @@
 public class App {
     public static void main(String[] args) throws Exception {
+         lesson_kanri lesson= new lesson_kanri();
+        lesson.start();
         shuseki shusseki= new shuseki();
         shusseki.start();
+       
+        
+        
 
     }
         
